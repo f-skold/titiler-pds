8efaef1bfe0c8e62a25df2608c33038130a948f6
Fredrik Skold <fredrik.skold@gmail.com>
2024-01-30 00:05:32 +0100

    More logging

--- titiler_pds/handler.py
+++ titiler_pds/handler.py
@@ -7,6 +7,6 @@ from mangum import Mangum
 from titiler_pds.main import app
 
 logging.getLogger("mangum.lifespan").setLevel(logging.ERROR)
-logging.getLogger("mangum.http").setLevel(logging.ERROR)
+logging.getLogger("mangum.http").setLevel(logging.INFO)
 
 handler = Mangum(app, lifespan="auto")
--- titiler_pds/main.py
+++ titiler_pds/main.py
@@ -4,6 +4,7 @@ import logging
 
 from brotli_asgi import BrotliMiddleware
 from fastapi import FastAPI
+from starlette import status
 from starlette.middleware.cors import CORSMiddleware
 from tilebench.middleware import VSIStatsMiddleware
 
@@ -13,15 +14,76 @@ from titiler.core.middleware import CacheControlMiddleware, TotalTimeMiddleware
 from .routes import landsat_collection2, naip, sentinel, skridskonet
 from .settings import api_config
 
-# turn off or quiet logs
-logging.getLogger("botocore.credentials").disabled = True
-logging.getLogger("botocore.utils").disabled = True
-# logging.getLogger("rio-tiler").setLevel(logging.ERROR)
-logging.getLogger("rio-tiler").setLevel(logging.DEBUG)
+logs = [
+    "asyncio",
+    "boto3.resources",
+    "boto3",
+    "botocore.auth",
+    "botocore.awsrequest",
+    "botocore.endpoint_provider",
+    "botocore.endpoint",
+    "botocore.handlers",
+    "botocore.loaders",
+    "botocore.regions",
+    "botocore.response",
+    "botocore.retries.adaptive",
+    "botocore.retries.special",
+    "botocore.retries.standard",
+    "botocore.retries",
+    "botocore.retryhandler" "botocore.session",
+    "botocore.tokens",
+    "botocore.utils",
+    "botocore",
+    "fastapi",
+    "rasterio.io",
+    "rasterio",
+    "rasterio._base",
+    "rasterio._env",
+    "rasterio._err",
+    "rio-tiler",
+]
+
+if False:
+    # turn off or quiet logs
+    logging.getLogger("botocore.credentials").disabled = True
+    logging.getLogger("botocore.utils").disabled = True
+    # logging.getLogger("rio-tiler").setLevel(logging.ERROR)
+    logging.getLogger("rio-tiler").setLevel(logging.DEBUG)
+else:
+    logging.getLogger("botocore.credentials").disabled = False
+    logging.getLogger("botocore.credentials").setLevel(logging.DEBUG)
+    logging.getLogger("botocore.utils").disabled = False
+    logging.getLogger("botocore.utils").setLevel(logging.DEBUG)
+    # logging.getLogger("rio-tiler").setLevel(logging.ERROR)
+    logging.getLogger("rio-tiler").setLevel(logging.DEBUG)
+    for name in logs:
+        logger = logging.getLogger(name)
+        # logger.disabled = False
+        logger.setLevel(logging.DEBUG)
+    if False:
+        print(f"Enabled logs for: {logs}")
 
 app = FastAPI(title="titiler-pds", version="0.1.0")
 
-add_exception_handlers(app, DEFAULT_STATUS_CODES)
+
+print("LOGGING")
+logger_names = [
+    "gunicorn.access",
+    "gunicorn.error",
+    "rio-tiler",
+    "uvicorn.access",
+    "uvicorn.error",
+]
+if False:
+    logger_names += list(logging.root.manager.loggerDict.keys())
+print(logger_names)
+
+some_exceptions = {
+    key: value
+    for key, value in DEFAULT_STATUS_CODES.items()
+    if value != status.HTTP_500_INTERNAL_SERVER_ERROR
+}
+add_exception_handlers(app, some_exceptions)
 if api_config.cors_origins:
     app.add_middleware(
         CORSMiddleware,
